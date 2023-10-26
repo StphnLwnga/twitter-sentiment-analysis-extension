3ec64ad9ac929a9ca86cc978fa14d4b8a16d901d
StphnLwnga <boywilder99@gmail.com>
2023-10-26 11:55:50 -0400

    text extraction for analysis

--- content_scripts/tweet-sentiment.js
+++ content_scripts/tweet-sentiment.js
@@ -17,3 +17,29 @@ function analyzeSentiment(text) {
     // return the calculated sentiment value to use somewhere else
     return sentiment;
 }
+
+// This function performs sentiment analysis on the text of a Twitter post
+// NOTE: This function does not handle the case with only an image, gif or video
+function categorizeTweet(tweet) {
+    // check if the tweet's DOM element already has been seen, if it has move on
+    if (tweet.hasAttribute("sentiment")) return;
+    // Grab all the span elements from the tweet (these contain all the text inside the tweet)
+    const spans = tweet.querySelectorAll("span");
+    // Create a spans array to hold all the different text in the spans
+    const spanTexts = [];
+    // Loop through each span and add them to the spanTexts array
+    spans.forEach((span) => {
+        spanTexts.push(span.innerText);
+    });
+    // Create a single string from all the elements in the array by joining each string. These are separated by a space.
+    const text = spanTexts.join(" ");
+    // Check if the text has been analyzed before if it has save the sentiment to the tweet's DOM element so it doesnt need to analyze it again
+    if (text in tweetSentiment) {
+        const sentiment = tweetSentiment[text];
+        tweet.setAttribute("sentiment", sentiment);
+        return;
+    }
+    // If the text hasnt been analyzed, do the sentiment analysis and save the sentiment to the DOM element
+    const sentiment = analyzeSentiment(text);
+    tweet.setAttribute("sentiment", sentiment);
+}
