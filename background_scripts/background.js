7bef7b2ec07f21bc900d4a53847b11faeec63a90
StphnLwnga <boywilder99@gmail.com>
2023-10-26 12:07:36 -0400

    Tally counts for each sentiment

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -2,6 +2,23 @@ let tweetSentiment = {};
 
 // NOTE FOR CHROME: please swap "browser" for "chrome"
 browser.runtime.onMessage.addListener(function (message) {
-  // Listen for the "sentiment" message, if seen update the `tweetSentiment` object to the data received
-  if (message.type === "sentiment") tweetSentiment = message.data;
+    // Listen for the "sentiment" message, if seen update the `tweetSentiment` object to the data received
+    if (message.type === "sentiment") tweetSentiment = message.data;
 });
+
+let sentimentValues = [];
+
+function countSentiments(obj) {
+    let values = Object.values(obj);
+
+    // Use the reduce method to count the number of occurrences of each value
+    let counts = values.reduce(
+        (acc, val) => {
+            acc[val == -1 ? String(val) : val]++;
+            return acc;
+        },
+        { "-1": 0, 0: 0, 1: 0 }
+    );
+
+    return Object.values(counts);
+}
